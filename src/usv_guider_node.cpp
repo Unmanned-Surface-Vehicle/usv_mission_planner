f7d521efe49454ca5c76a2802c95f6f0b8b01389
DarlanJurak <darlan.jurak@gmail.com>
2019-04-15 12:55:04 -0300

    added configuration for catkin_make_isolated installation

--- src/usv_guider_node.cpp
+++ src/usv_guider_node.cpp
@@ -10,20 +10,24 @@
 
 void Test0_AStar(int, int, int, int);
 
+#define PUBLISH_TIME_INTERVAL___USV_GUIDER___WAYPOINT_COMMAND 0.2
+#define PUBLISH_QUEUE_AMOUNT___USV_GUIDER___WAYPOINT_COMMAND  10
+
 int main(int argc, char **argv)
 {
 
   /*
   * Basic configuration to start node and configure a publisher
   */
-  ros::init(argc, argv, "usv_guider_node");                                                   // Basic iniitalization for ROS node
-  ros::NodeHandle n;                                                                          // Handler to interact with ROS
-  ros::Publisher usv_guider_pub = n.advertise<nav_msgs::Odometry>("/airboat/move_usv/goal", 10);        // Declaration of ROS topic and creation of a publishing handler
-  ros::Rate loop_rate(0.2);                                                                   // Runs CA strategy each 5 secs.  
+  ros::init(argc, argv, "usv_guider_node");                                                                                                         // Basic iniitalization for ROS node
+  ros::NodeHandle n;                                                                                                                                // Handler to interact with ROS
+  ros::Publisher usv_guider_pub = n.advertise<nav_msgs::Odometry>("/airboat/move_usv/goal", PUBLISH_QUEUE_AMOUNT___USV_GUIDER___WAYPOINT_COMMAND);  // Declaration of ROS topic and creation of a publishing handler for usv_guider waypoint command 
+  ros::Rate loop_rate(PUBLISH_TIME_INTERVAL___USV_GUIDER___WAYPOINT_COMMAND);                                                                       // Runs CA strategy each 1/PUBLISH_TIME_INTERVAL___USV_GUIDER___WAYPOINT_COMMAND secs.  
 
-  nav_msgs::Odometry usv_guider_command_position_msg;
+  nav_msgs::Odometry usv_guider_command_position_msg;               // USV Guider position command message
   usv_guider_command_position_msg.header.stamp = ros::Time::now();
-  usv_guider_command_position_msg.header.frame_id  = "world";
+  usv_guider_command_position_msg.header.frame_id  = "world";  
+
   geometry_msgs::Point point;
   point.x = 255;
   point.y = 0;
