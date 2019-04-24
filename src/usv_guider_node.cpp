6cc35033e255d7b71545940dc1d641a5df1409cc
DarlanJurak <darlan.jurak@gmail.com>
2019-04-24 18:02:52 -0300

    v0.1 Integration USV_SIM and USV_GUIDER

--- src/usv_guider_node.cpp
+++ src/usv_guider_node.cpp
@@ -8,20 +8,22 @@
 
 #include <sstream>
 
+#define ROS_MAIN_LOOP___TIME_INTERVAL                         0.2
+#define PUBLISH_QUEUE_AMOUNT___USV_GUIDER___WAYPOINT_COMMAND  10
+#define SUBSCRIBE_QUEUE_AMOUNT___USV___CURRENT_POSITION       1
+#define ACCEPTABLE_RADIUS                                     2
+
 std::queue<Pos>* A_Star();
 void usv_state_callback(const nav_msgs::Odometry::ConstPtr& usv_position_msg);
 
-#define ROS_MAIN_LOOP___TIME_INTERVAL                         0.2
-#define PUBLISH_QUEUE_AMOUNT___USV_GUIDER___WAYPOINT_COMMAND  10
-#define SUBSCRIBE_QUEUE_AMOUNT___USV___CURRENT_POSITION       1000
-#define ACCEPTABLE_RADIUS                                     2
+geometry_msgs::Point usv_current_pos;
 
 int main(int argc, char **argv){
   
   // Basic configuration to start node and configure a publisher
   ros::init(argc, argv, "usv_guider_node");                                                                                                         // Basic iniitalization for ROS node
   ros::NodeHandle n;                                                                                                                                // Handler to interact with ROS
-  ros::Publisher usv_guider_pub   = n.advertise<nav_msgs::Odometry>("/airboat/move_usv/goal", PUBLISH_QUEUE_AMOUNT___USV_GUIDER___WAYPOINT_COMMAND);// Declaration of ROS topic and creation of a publishing handler for usv_guider waypoint command 
+  ros::Publisher  usv_guider_pub  = n.advertise<nav_msgs::Odometry>("/airboat/move_usv/goal", PUBLISH_QUEUE_AMOUNT___USV_GUIDER___WAYPOINT_COMMAND);// Declaration of ROS topic and creation of a publishing handler for usv_guider waypoint command 
   ros::Subscriber usv_guider_sub  = n.subscribe("/airboat/state", SUBSCRIBE_QUEUE_AMOUNT___USV___CURRENT_POSITION, usv_state_callback);
   ros::Rate loop_rate(ROS_MAIN_LOOP___TIME_INTERVAL);                                                                                               // Runs CA strategy each 1/PUBLISH_TIME_INTERVAL___USV_GUIDER___WAYPOINT_COMMAND secs.  
 
@@ -30,44 +32,54 @@ int main(int argc, char **argv){
   usv_guider_command_position_msg.header.stamp = ros::Time::now();
   usv_guider_command_position_msg.header.frame_id  = "world";  
 
-  // Target position goal for usv
-  geometry_msgs::Point point;
-  point.x = 240;
-  point.y = 95;
-  point.z = 0;
-  usv_guider_command_position_msg.pose.pose.position = point;
+  // Gets A* output path
+  std::queue<Pos> *path = A_Star();
 
-  std::queue<Pos> *path;
-  path = A_Star();
-  Pos test;
-
-  Pos next_goal = path->front();
+  // Gets first subgoal location
+  Pos next_goal;
+  next_goal.x = path->front().x;
+  next_goal.y = path->front().y;
   path->pop();
 
-  while (ros::ok())
-  {
+  // Handler to publish usv subgoal location
+  geometry_msgs::Point point;
+  point.x = next_goal.x;
+  point.y = next_goal.y;
+  point.z = 0;
 
-    // while(1){ //while(usv.currentPos() != usv_guider.getFinalGoal())
+  // publish first subgoal location
+  usv_guider_command_position_msg.pose.pose.position = point;
+  usv_guider_pub.publish(usv_guider_command_position_msg);
 
-      // if(usv_current_pos.x != next_goal.x || usv_current_pos.y != next_goal.y){
+  while (ros::ok()){
 
-      //   if(!path->empty()){
+    ROS_INFO("Current x: %f y: %f",   usv_current_pos.x, usv_current_pos.y);
+    ROS_INFO("Next    x: %f y: %f\n", next_goal.x, next_goal.y);
 
-      //     next_goal = path->front();
-      //     path->pop();
+    // Set new subgoal location after reach current subgoal
+    if((abs(usv_current_pos.x - next_goal.x) <= ACCEPTABLE_RADIUS) && (abs(usv_current_pos.y - next_goal.y) <= ACCEPTABLE_RADIUS)){ // Tests if achieved subgoal
 
-      //     point.x = next_goal.x;
-      //     point.y = next_goal.y;
-      //     usv_guider_command_position_msg.pose.pose.position = point;
+      if(!path->empty()){
 
-      //   }
+        next_goal = path->front();                                  // Gets next subgoal location
+        path->pop();                                                // Removes from path data structure
 
-      // }
+        // Update subgoal
+        point.x = next_goal.x;
+        point.y = next_goal.y;
+        usv_guider_command_position_msg.pose.pose.position = point;
 
-    // }
-    
-    ROS_INFO("Test >>>");
+      }else{
+
+        delete path;
+
+      }
+
+    }
+
     usv_guider_pub.publish(usv_guider_command_position_msg);
+
+    // Attending to subscription callbacks
     ros::spinOnce();
     loop_rate.sleep();
 
@@ -81,9 +93,8 @@ int main(int argc, char **argv){
 
 void usv_state_callback(const nav_msgs::Odometry::ConstPtr& usv_position_msg){
 
-  geometry_msgs::Point usv_current_pos;
   usv_current_pos = usv_position_msg->pose.pose.position;
-  ROS_INFO("USV current position: X: %f, Y: %f", usv_current_pos.x, usv_current_pos.y);
+  // ROS_INFO("USV current position: X: %f, Y: %f", usv_current_pos.x, usv_current_pos.y);
   // ROS_INFO("C_x - N_x: %f - %f --- C_y - N_y: %f - %f", usv_current_pos.x, next_goal.x, usv_current_pos.y, next_goal.y);
 
 }
@@ -94,12 +105,30 @@ std::queue<Pos>* A_Star(){
   Pos test;
 
   test.x = 240;
-  test.y = 110;
+  test.y = 95;
+  path->push(test);
+
+  test.x = 260;
+  test.y = 95;
   path->push(test);
 
   test.x = 240;
-  test.y = 80;
+  test.y = 95;
   path->push(test);
 
+  test.x = 260;
+  test.y = 95;
+  path->push(test);
+
+  test.x = 240;
+  test.y = 95;
+  path->push(test);
+
+  test.x = 260;
+  test.y = 95;
+  path->push(test);
+  
+
   return path;
+  
 }
